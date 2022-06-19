772d0b1c15c161c3917545c8aa825b52faecda3f
Michal Tangri <mt.michaltangri@gmail.com>
2022-06-19 21:16:37 +0200

    cleanup

--- App.js
+++ App.js
@@ -1,26 +1,8 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { HomeScreen } from './components/homeScreen.component';
 import { HealthScreen } from './components/healthScreen.component';
-
-// export function HomeScreen({ navigation }) {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Home Screen</Text>
-//       <Button title="Go to Details" onPress={() => navigation.navigate('Health')} />
-//     </View>
-//   );
-// }
-
-// export function HealthScreen() {
-//   return (
-//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//       <Text>Details Screen</Text>
-//     </View>
-//   );
-// }
 
 const Stack = createNativeStackNavigator();
 
