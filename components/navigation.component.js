5fee5cda6e10b3d5464e53ea48e57528397ef514
Michal Tangri <mt.michaltangri@gmail.com>
2022-06-22 18:51:08 +0200

    PAMO-15: roll me baby dawaj bata

--- components/navigation.component.js
+++ components/navigation.component.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Icon from 'react-native-vector-icons/FontAwesome5';
@@ -18,14 +17,26 @@ export class Navigation extends React.Component {
   render() {
     return (
       <this.Tab.Navigator initialRouteName="Home" backBehavior="history">
-        <this.Tab.Screen name={NavigationEnum.Home} component={HomeScreen} options={{ tabBarIcon: () => getIconComponent('home') }} />
-        <this.Tab.Screen name={NavigationEnum.Health} component={HealthScreen} options={{ tabBarIcon: () => getIconComponent('heart') }} />
-        <this.Tab.Screen name={NavigationEnum.Dice} component={DiceScreen} options={{ tabBarIcon: () => getIconComponent('dice') }} />
+        <this.Tab.Screen
+          name={NavigationEnum.Home}
+          component={HomeScreen}
+          options={{ header: () => null, tabBarIcon: () => getIconComponent('home') }}
+        />
+        <this.Tab.Screen
+          name={NavigationEnum.Health}
+          component={HealthScreen}
+          options={{ header: () => null, tabBarIcon: () => getIconComponent('heart') }}
+        />
+        <this.Tab.Screen
+          name={NavigationEnum.Dice}
+          options={{ header: () => null, tabBarIcon: () => getIconComponent('dice') }}
+          component={DiceScreen}
+        />
       </this.Tab.Navigator>
     );
   }
 }
 
-function getIconComponent(name, size = 25) {
-  return <Icon name={name} size={size} />;
+function getIconComponent(name, size = 25, color = 'crimson', solid = true) {
+  return <Icon name={name} size={size} color={color} solid={solid} />;
 }
